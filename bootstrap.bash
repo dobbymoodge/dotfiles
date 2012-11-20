e18cc97750d7586294d93780a6be196861b1aa69
John Lamb <john.w.lamb@gmail.com>
2012-11-20 17:07:14 -0500

    updated bootstrap.bash to be slightly more smarty

--- bootstrap.bash
+++ bootstrap.bash
@@ -2,8 +2,20 @@
 
 DOTFILES="${HOME}/.dotfiles"
 
+do_link () {
+    if [ -e "$2" ] ; then
+	if ! [ -L "$2" ] ; then
+	    echo "$2 exists, but is not a link!"
+	else 
+	    echo "$2 exists"
+	fi
+    else
+	ln -s "$1" "$2"
+    fi
+}
+
 # Are we in the .dotfiles directory?
 if [ -d ./.git -a "${PWD}" == "${DOTFILES}" ] ; then
-    ln -s "${DOTFILES}/.config/subtle" "${HOME}/.config/subtle"
-    ln -s "${DOTFILES}/.Xresources" "${HOME}/.Xresources"
+    do_link "${DOTFILES}/.config/subtle" "${HOME}/.config/subtle"
+    do_link "${DOTFILES}/.Xresources" "${HOME}/.Xresources"
 fi
