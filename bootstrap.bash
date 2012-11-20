d28874acba9eb2fa202bfc418e1cfd1921518d49
John Lamb <john.w.lamb@gmail.com>
2012-11-20 16:58:32 -0500

    updated bootstrap to include link for .Xresources

--- bootstrap.bash
+++ bootstrap.bash
@@ -5,4 +5,5 @@ DOTFILES="${HOME}/.dotfiles"
 # Are we in the .dotfiles directory?
 if [ -d ./.git -a "${PWD}" == "${DOTFILES}" ] ; then
     ln -s "${DOTFILES}/.config/subtle" "${HOME}/.config/subtle"
+    ln -s "${DOTFILES}/.Xresources" "${HOME}/.Xresources"
 fi
