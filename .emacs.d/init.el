a21e4c31d4c5f7e3578f263b032b801e7464dbd6
John Lamb <john.w.lamb@gmail.com>
2012-12-09 14:58:35 -0500

    add ido-mode to emacs init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,9 @@
                "\\documentclass{article}"
                ("\\section{%s}" . "\\section*{%s}")))
 
+(require 'ido)
+(ido-mode t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
