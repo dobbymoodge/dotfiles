27841155dc9f115906e5b93c63da22d0eb96d789
dobbymoodge <jolamb@redhat.com>
2018-05-23 14:07:59 -0400

    Add hostname-keyed custom files

--- bootstrap.bash
+++ bootstrap.bash
@@ -1,31 +1,49 @@
 #!/bin/bash
-
-DOTFILES="${HOME}/.dotfiles"
-[[ -e "${HOME}/dotfiles" ]] && DOTFILES="${HOME}/dotfiles"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOTFILES="${DIR}"
+HOSTPATH="${DOTFILES}/hosts/${HOSTNAME}"
+# [[ -e "${HOME}/dotfiles" ]] && DOTFILES="${HOME}/dotfiles"
 
 do_link () {
     if [ -e "$2" ] ; then
 	    if ! [ -L "$2" ] ; then
 	        echo "$2 exists, but is not a link!"
-	    else 
+	    else
 	        echo "$2 exists"
 	    fi
         return 1
     else
-	    ln -s "$1" "$2"
+	  # ln -s "$1" "$2"
+      echo "ln -s \"$1\" \"$2\""
     fi
 }
 
+if [[ -d "${HOSTPATH}" ]] ; then
+  host_link () {
+    if [[ -e "${HOSTPATH}/${1}" ]] ; then
+      do_link "${HOSTPATH}/${1}" "${2}"
+    else
+      do_link "${DOTFILES}/${1}" "${2}"
+    fi
+  }
+else
+  host_link () {
+    do_link "${DOTFILES}/${1}" "${2}"
+  }
+fi
+
+# eval "$(echo "orig_theirfunc()"; declare -f theirfunc | tail -n +2)"
+
 # Are we in the .dotfiles directory?
 if [ -d ./.git -a "${PWD}" == "${DOTFILES}" ] ; then
-#    do_link "${DOTFILES}/.config/subtle" "${HOME}/.config/subtle"
-#    if do_link "${DOTFILES}/.emacs.d" "${HOME}/.emacs.d" ; then
-#        do_link "${DOTFILES}/.emacs.d/init.el" "${HOME}/.emacs"
+#    host_link ".config/subtle" "${HOME}/.config/subtle"
+#    if host_link ".emacs.d" "${HOME}/.emacs.d" ; then
+#        host_link ".emacs.d/init.el" "${HOME}/.emacs"
 #    fi
-    do_link "${DOTFILES}/.Xresources" "${HOME}/.Xresources"
-    do_link "${DOTFILES}/.tmux.conf" "${HOME}/.tmux.conf"
-    do_link "${DOTFILES}/git/.gitconfig" "${HOME}/.gitconfig"
-    do_link "${DOTFILES}/git/.git-templates" "${HOME}/.git-templates"
+    host_link ".Xresources" "${HOME}/.Xresources"
+    host_link ".tmux.conf" "${HOME}/.tmux.conf"
+    host_link "git/.gitconfig" "${HOME}/.gitconfig"
+    host_link "git/.git-templates" "${HOME}/.git-templates"
 
-#    do_link "${DOTFILES}/.fvwm" "${HOME}/.fvwm"
+    host_link ".fvwm" "${HOME}/.fvwm"
 fi
